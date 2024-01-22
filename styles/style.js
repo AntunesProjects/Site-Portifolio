99a0fe256c5753e44fd1df930e4a66c8d6349728
AntunesProjects <antunesmatheus.contato@gmail.com>
2024-01-22 18:24:40 -0300

    aside

--- styles/style.js
+++ styles/style.js
@@ -1,5 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const asideContainer = document.querySelector(".aside-container");
 
-//rolamento suave
+  window.addEventListener("scroll", function() {
+    const scrollPercentage = (window.scrollY / window.innerHeight) * 100;
+
+    if (scrollPercentage >= 100) {
+      mostrarAsideContainer();
+    } else if (scrollPercentage < 100) {
+      esconderAsideContainer();
+    }
+  });
+
+  function esconderAsideContainer() {
+    if (asideContainer) {
+      asideContainer.style.display = "none";
+    }
+  }
+
+  function mostrarAsideContainer() {
+    if (asideContainer) {
+      asideContainer.style.display = "block";
+    }
+  }
+});
+
+
+//Rolamento suave:
 function smoothScroll(targetId) {
     const targetElement = document.querySelector(targetId);
     

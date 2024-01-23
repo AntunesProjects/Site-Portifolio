8487d19296d897b4464e3389a0bdb8d23cef8e05
AntunesProjects <antunesmatheus.contato@gmail.com>
2024-01-23 10:24:46 -0300

    aside estilizado e redimensionamento

--- styles/style.js
+++ styles/style.js
@@ -1,28 +1,31 @@
+//Animação da barra lateral
 document.addEventListener("DOMContentLoaded", function() {
-  const asideContainer = document.querySelector(".aside-container");
+    const asideContainer = document.querySelector(".aside-container");
 
-  window.addEventListener("scroll", function() {
-    const scrollPercentage = (window.scrollY / window.innerHeight) * 100;
+    window.addEventListener("scroll", function() {
+        const scrollPercentage = (window.scrollY / window.innerHeight) * 100;
 
-    if (scrollPercentage >= 100) {
-      mostrarAsideContainer();
-    } else if (scrollPercentage < 100) {
-      esconderAsideContainer();
+        if (scrollPercentage >= 80) {
+            mostrarAsideContainer();
+        } else if (scrollPercentage < 80) {
+            esconderAsideContainer();
+        }
+    });
+
+    function esconderAsideContainer() {
+        if (asideContainer) {
+            asideContainer.style.left = '-20vw'; // Move para fora da tela
+        }
     }
-  });
 
-  function esconderAsideContainer() {
-    if (asideContainer) {
-      asideContainer.style.display = "none";
+    function mostrarAsideContainer() {
+        if (asideContainer) {
+            asideContainer.style.left = '0.4vw'; // Traga de volta para a tela
+        }
     }
-  }
-
-  function mostrarAsideContainer() {
-    if (asideContainer) {
-      asideContainer.style.display = "block";
-    }
-  }
 });
+
+
 
 
 //Rolamento suave:

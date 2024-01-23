6c54c3f6c9c655336342de38363d5b21f52e4bc4
AntunesProjects <antunesmatheus.contato@gmail.com>
2024-01-23 16:51:19 -0300

    modo noturno

--- styles/style.js
+++ styles/style.js
@@ -25,9 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-
-
-
 //Rolamento suave:
 function smoothScroll(targetId) {
     const targetElement = document.querySelector(targetId);
@@ -40,8 +37,58 @@ function smoothScroll(targetId) {
     }
 }
 
-
 //Onclick
-    function redirecionarParaURL(url) {
-        window.location.href = url;
+function redirecionarParaURL(url) {
+    window.location.href = url;
+}
+
+//Modo escuro
+function alternarModoNoturno() {
+    var modoNoturnoAtivo = document.body.classList.toggle('modo-noturno');
+
+    atualizarVariaveisCSS(modoNoturnoAtivo);
+}
+
+function atualizarVariaveisCSS(modoNoturnoAtivo) {
+    var root = document.documentElement;
+
+    if (modoNoturnoAtivo) {
+        // Modo noturno ativado, atualize as variáveis para as cores escuras
+        root.style.setProperty('--color-a', '#353535');
+        root.style.setProperty('--color-b', '#111111');
+        root.style.setProperty('--color-c', '#dee2e6');
+        root.style.setProperty('--color-d', '#fafafa');
+        root.style.setProperty('--color-e', '#141414');
+        root.style.setProperty('--color-f', '#ffffff');
+        root.style.setProperty('--color-g', '#204766');
+        root.style.setProperty('--color-h', '#336792');
+        root.style.setProperty('--color-i', '#353535');
+
+        // Adicione a classe modo-noturno aos elementos com ícones SVG
+        document.querySelectorAll('.aside-container-icons a svg').forEach(function (icon) {
+            icon.classList.add('modo-noturno');
+        });
+        document.querySelectorAll('.contato-container-icones a svg').forEach(function (icon) {
+            icon.classList.add('modo-noturno');
+        });
+    } else {
+        // Modo noturno desativado, volte às cores originais
+        root.style.setProperty('--color-a', '#dee2e6');
+        root.style.setProperty('--color-b', '#fafafa');
+        root.style.setProperty('--color-c', '#353535');
+        root.style.setProperty('--color-d', '#141414');
+        root.style.setProperty('--color-e', '#ffffff');
+        root.style.setProperty('--color-f', '#152e42');
+        root.style.setProperty('--color-g', '#204766');
+        root.style.setProperty('--color-h', '#336792');
+        root.style.setProperty('--color-i', '#ffffff');
+
+        // Remova a classe modo-noturno dos elementos com ícones SVG
+        document.querySelectorAll('.aside-container-icons a svg').forEach(function (icon) {
+            icon.classList.remove('modo-noturno');
+        });
+        document.querySelectorAll('.contato-container-icones a svg').forEach(function (icon) {
+            icon.classList.add('modo-noturno');
+        });
     }
+}
